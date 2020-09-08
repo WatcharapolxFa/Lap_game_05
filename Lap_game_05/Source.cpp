70124eebd5b68dcd6d42c40c6fa4c30fcb97e520
WatcharapolxFa <69455513+WatcharapolxFa@users.noreply.github.com>
2020-09-08 11:03:46 +0700

    20 %

---
+++ Lap_game_05/Source.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+void draw_ship(int, int);
+void gotoxy(int, int);
+void erase_ship(int, int);
+void setcursor(bool);
+void setcolor(int, int);
+
+int main()
+
+{
+	char ch = ' ';
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a' && x > 0) { erase_ship(x, y); draw_ship(--x, y); }
+			if (ch == 'd' && x < 113) { erase_ship(x, y); draw_ship(++x, y); }
+			if (ch == 'w' && y > 0) { erase_ship(x, y); draw_ship(x, --y); }
+			if (ch == 's' && y < 30) { erase_ship(x, y); draw_ship(x, ++y); }
+			fflush(stdin);
+		}
+		Sleep(500);
+		setcolor(2, 0);
+	} while (ch != 'x');
+	return 0;
+}
+
+void draw_ship(int x, int y)
+{
+	setcursor(0);
+	setcolor(2, 4);
+	gotoxy(x, y);
+	printf(" <-0-> ");
+}
+
+void erase_ship(int x, int y)
+{
+	gotoxy(x, y);
+	printf("       ");
+	
+}
+
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void setcursor(bool visible)
+{
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO lpCursor;
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = 20;
+	SetConsoleCursorInfo(console, &lpCursor);
+}
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
